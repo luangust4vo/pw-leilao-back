9b02674320c0ffa5158707909cd112baab7d0f1e
Luan Gustavo <gluan1213@gmail.com>
2025-06-09 19:57:32 -0300

    chore: start project

---
+++ src/main/java/com/github/luangust4vo/pw_leilao_backend/PwLeilaoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.luangust4vo.pw_leilao_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwLeilaoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwLeilaoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/luangust4vo/pw_leilao_backend/PwLeilaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.luangust4vo.pw_leilao_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwLeilaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
