6dc54d28235d1fc07d85e9956c919d02f9aa95c5
Luan Gustavo <gluan1213@gmail.com>
2025-07-26 20:58:11 -0300

    chore: updates the person model to match the class diagram

--- src/main/java/com/github/luangust4vo/pw_leilao_backend/controllers/PersonController.java
+++ src/main/java/com/github/luangust4vo/pw_leilao_backend/controllers/PersonController.java
@@ -1,7 +1,5 @@
 package com.github.luangust4vo.pw_leilao_backend.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
--- src/main/java/com/github/luangust4vo/pw_leilao_backend/models/Person.java
+++ src/main/java/com/github/luangust4vo/pw_leilao_backend/models/Person.java
@@ -1,10 +1,15 @@
 package com.github.luangust4vo.pw_leilao_backend.models;
 
+import java.util.Date;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -17,12 +22,47 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name = "name", nullable = false)
     @NotBlank(message = "{validation.name.not-blank}")
     private String name;
+
     @Column(name = "email", nullable = false, unique = true)
     @NotBlank(message = "{validation.email.not-blank}")
     @Email(message = "{validation.email.invalid}")
     private String email;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "validation_code", nullable = true, unique = true)
+    private String validationCode;
+
+    @Column(name = "validation_code_expiration", nullable = true)
+    private Date validationCodeExpiration;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive = false;
+
+    @Lob
+    @Column(name = "perfil_image", nullable = true)
+    private byte[] perfilImage;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        Date now = new Date();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = new Date();
+    }
 }
