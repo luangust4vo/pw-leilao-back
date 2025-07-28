a6df7012997eb3eeb86d034fb2597b37db818abb
Luan Gustavo <gluan1213@gmail.com>
2025-07-27 23:15:10 -0300

    feat(domain): Create AuctionStatus and PerfilType enums

---
+++ src/main/java/com/github/luangust4vo/pw_leilao_backend/models/enums/AuctionStatus.java
@@ -0,0 +1,8 @@
+package com.github.luangust4vo.pw_leilao_backend.models.enums;
+
+public enum AuctionStatus {
+    ABERTO,
+    ENCERRADO,
+    CANCELADO,
+    EM_ANALISE
+}
---
+++ src/main/java/com/github/luangust4vo/pw_leilao_backend/models/enums/PerfilType.java
@@ -0,0 +1,7 @@
+package com.github.luangust4vo.pw_leilao_backend.models.enums;
+
+public enum PerfilType {
+    ADMIN,
+    COMPRADOR,
+    VENDEDOR
+}
